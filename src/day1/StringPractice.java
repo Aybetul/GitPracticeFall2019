9de56de4c8e07eb1cab42d35027b621335e736e0
Aybetul <aybetul@hotmail.com>
2020-02-29 15:26:49 -0500

    modified StringPractice class

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,6 +3,8 @@ package day1;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("Lets see what is going to happen");
+
     }
 
 }
